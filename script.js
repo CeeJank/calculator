41b015b9d591d38528e68fc1b81e315eb24b7d76
CeeJank <90126284+CeeJank@users.noreply.github.com>
2024-10-03 22:48:16 +0800

    added some selectors

---
+++ script.js
@@ -0,0 +1,24 @@
+//assign all selectors
+const output = document.querySelector(".output");
+
+//functions
+arr = []
+const num = document.querySelectorAll(".num");
+num.forEach(number => {
+    number.addEventListener("click",appendToOutput())
+});
+
+function appendToOutput() {
+    return
+}
+
+
+
+
+
+
+//calculator answer
+const answer = document.querySelector("#=")
+answer.addEventListener("click", function() {
+    return;
+})
